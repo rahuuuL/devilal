1bbe7562478f6539788032952c78a4e36b042d35
rahul <s.rahul0597@gmail.com>
2025-05-07 01:26:32 +0530

    Kafka Development

---
+++ src/main/java/com/terminal_devilal/configurations/KafkaProducerConfig.java
@@ -0,0 +1,31 @@
+package com.terminal_devilal.configurations;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.annotation.EnableKafka;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Configuration
+@EnableKafka
+public class KafkaProducerConfig {
+	
+	@Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        Map<String, Object> producerProps = new HashMap<>();
+        producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        ProducerFactory<String, String> producerFactory = new DefaultKafkaProducerFactory<>(producerProps);
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+}
