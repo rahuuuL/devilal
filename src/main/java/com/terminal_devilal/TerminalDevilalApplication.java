83d47170f0d932de47efbdb970b47571b7bd4659
rahul <s.rahul0597@gmail.com>
2025-05-25 22:17:40 +0530

    kafak integration development

--- src/main/java/com/terminal_devilal/TerminalDevilalApplication.java
+++ src/main/java/com/terminal_devilal/TerminalDevilalApplication.java
@@ -1,23 +1,12 @@
 package com.terminal_devilal;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.terminal_devilal.configurations.KafkaProducerService;
-
 @SpringBootApplication
 public class TerminalDevilalApplication {
-	
-    @Autowired
-    private static KafkaProducerService producerService;
-    
 
 	public static void main(String[] args) {
 		SpringApplication.run(TerminalDevilalApplication.class, args);
 	}
-	
-    public void run(String... args) throws Exception {
-        producerService.sendMessage("test-topic", "Hello Kafka!");
-    }
 }
--- src/main/java/com/terminal_devilal/configurations/KafkaMessageConsumer.java
+++ src/main/java/com/terminal_devilal/configurations/KafkaMessageConsumer.java
@@ -1,13 +1,13 @@
-package com.terminal_devilal.configurations;
-
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Service;
-
-@Service
-public class KafkaMessageConsumer {
-
-    @KafkaListener(topics = "test-topic", groupId = "my-consumer-group")
-    public void listen(String message) {
-        System.out.println("Received Message: " + message);
-    }
-}
+//package com.terminal_devilal.configurations;
+//
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class KafkaMessageConsumer {
+//
+//    @KafkaListener(topics = "test-topic", groupId = "my-consumer-group")
+//    public void listen(String message) {
+//        System.out.println("Received Message: " + message);
+//    }
+//}
