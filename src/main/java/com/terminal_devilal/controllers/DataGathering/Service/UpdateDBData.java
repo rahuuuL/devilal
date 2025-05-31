76fc9c01f07fd998f5959852f2857fd8c586159d
rahul <s.rahul0597@gmail.com>
2025-06-01 01:13:18 +0530

    commented unwanted code

--- src/main/java/com/terminal_devilal/controllers/DataGathering/Service/UpdateDBData.java
+++ src/main/java/com/terminal_devilal/controllers/DataGathering/Service/UpdateDBData.java
@@ -1,32 +1,32 @@
-package com.terminal_devilal.controllers.DataGathering.Service;
-
-import java.util.List;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UpdateDBData {
-
-	private ProcessedDatesService processedDatesService;
-
-	private List<String> links = List.of(
-			"https://www.nseindia.com/api/historical/securityArchives?from=%s&to=%s&symbol=%s&dataType=priceVolumeDeliverable&series=EQ");
-
-	public UpdateDBData(ProcessedDatesService processedDatesService) {
-		this.processedDatesService = processedDatesService;
-	}
-
-//	public void updateData() throws IOException, InterruptedException {
-//	    List<DataFetchHistroy> datesForProcessing = processedDatesService.getDatesForProcessing();
+//package com.terminal_devilal.controllers.DataGathering.Service;
 //
-//	    for (DataFetchHistroy processedDateEntry : datesForProcessing) {
-//	        for (String apiLinkTemplate : links) {
-//	            String fromDate = processedDateEntry.getProcessedDate().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
-//	            String today = LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
-//	            String formattedUrl = String.format(apiLinkTemplate, fromDate, today, processedDateEntry.getTicker());
+//import java.util.List;
+//import org.springframework.stereotype.Service;
 //
-//	            FetchNSEAPI.NSEAPICall(formattedUrl);
-//	        }
-//	    }
+//@Service
+//public class UpdateDBData {
+//
+//	private ProcessedDatesService processedDatesService;
+//
+//	private List<String> links = List.of(
+//			"https://www.nseindia.com/api/historical/securityArchives?from=%s&to=%s&symbol=%s&dataType=priceVolumeDeliverable&series=EQ");
+//
+//	public UpdateDBData(ProcessedDatesService processedDatesService) {
+//		this.processedDatesService = processedDatesService;
 //	}
-
-}
+//
+////	public void updateData() throws IOException, InterruptedException {
+////	    List<DataFetchHistroy> datesForProcessing = processedDatesService.getDatesForProcessing();
+////
+////	    for (DataFetchHistroy processedDateEntry : datesForProcessing) {
+////	        for (String apiLinkTemplate : links) {
+////	            String fromDate = processedDateEntry.getProcessedDate().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+////	            String today = LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+////	            String formattedUrl = String.format(apiLinkTemplate, fromDate, today, processedDateEntry.getTicker());
+////
+////	            FetchNSEAPI.NSEAPICall(formattedUrl);
+////	        }
+////	    }
+////	}
+//
+//}
--- src/test/java/com/terminal_devilal/TerminalDevilalApplicationTests.java
+++ src/test/java/com/terminal_devilal/TerminalDevilalApplicationTests.java
@@ -1,13 +1,13 @@
-package com.terminal_devilal;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class TerminalDevilalApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.terminal_devilal;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class TerminalDevilalApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
