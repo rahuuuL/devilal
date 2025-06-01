ab413cf5422bdff1d1327976f5a134160d44cdd3
rahul <s.rahul0597@gmail.com>
2025-06-01 16:08:44 +0530

    code refactor and clean code

--- src/main/java/com/terminal_devilal/controllers/DataGathering/Service/PriceDeliveryVolumeService.java
+++ src/main/java/com/terminal_devilal/controllers/DataGathering/Service/PriceDeliveryVolumeService.java
@@ -6,14 +6,14 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 import java.util.TreeSet;
-import java.util.concurrent.atomic.AtomicReference;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.terminal_devilal.Utils.KafkaUtils;
+import com.terminal_devilal.configurations.KafkaProducerService;
 import com.terminal_devilal.controllers.DataGathering.DAO.PriceDeliveryVolumeDAO;
+import com.terminal_devilal.controllers.DataGathering.Exception.DateParserException;
 import com.terminal_devilal.controllers.DataGathering.Model.PriceDeliveryVolume;
 
 @Service
@@ -21,12 +21,12 @@ public class PriceDeliveryVolumeService {
 
 	private final PriceDeliveryVolumeDAO repository;
 
-	private final KafkaUtils kafkaUtils;
+	private final KafkaProducerService kafkaProducerService;
 
-	public PriceDeliveryVolumeService(PriceDeliveryVolumeDAO repository, KafkaUtils kafkaUtils) {
+	public PriceDeliveryVolumeService(PriceDeliveryVolumeDAO repository, KafkaProducerService kafkaProducerService) {
 		super();
 		this.repository = repository;
-		this.kafkaUtils = kafkaUtils;
+		this.kafkaProducerService = kafkaProducerService;
 	}
 
 	@Transactional
@@ -47,7 +47,7 @@ public class PriceDeliveryVolumeService {
 	private final DateTimeFormatter NSE_DATE_FORMATTER = DateTimeFormatter.ofPattern("dd-MMM-yyyy", Locale.ENGLISH);
 
 	// Mapper Utility function
-	public TreeSet<PriceDeliveryVolume> parseStockData(JsonNode node, String ticker) {
+	public TreeSet<PriceDeliveryVolume> parseStockDataAndProduce(JsonNode node, String ticker) {
 
 		TreeSet<PriceDeliveryVolume> stockList = new TreeSet<PriceDeliveryVolume>(
 				Comparator.comparing(PriceDeliveryVolume::getDate));
@@ -58,60 +58,44 @@ public class PriceDeliveryVolumeService {
 			return stockList; // return empty if "data" is not an array
 		}
 
-		AtomicReference<String> lastTicker = new AtomicReference<>(null);
-
 		for (JsonNode item : dataArray) {
-			PriceDeliveryVolume stock = new PriceDeliveryVolume();
-
+			PriceDeliveryVolume stock = parseStockData(item);
 			stock.setTicker(ticker);
-			stock.setDate(parseDate(item.path("mTIMESTAMP").asText("")));
-			stock.setHigh(item.path("CH_TRADE_HIGH_PRICE").asDouble(0.0));
-			stock.setLow(item.path("CH_TRADE_LOW_PRICE").asDouble(0.0));
-			stock.setOpen(item.path("CH_OPENING_PRICE").asDouble(0.0));
-			stock.setClose(item.path("CH_CLOSING_PRICE").asDouble(0.0));
-			stock.setLastTradeValue(item.path("CH_LAST_TRADED_PRICE").asDouble(0.0));
-			stock.setPrevoiusClosePrice(item.path("CH_PREVIOUS_CLS_PRICE").asDouble(0.0));
-			stock.setVolume(item.path("CH_TOT_TRADED_QTY").asLong(0L));
-			stock.setValue(item.path("CH_TOT_TRADED_VAL").asDouble(0.0));
-			stock.setTrades(item.path("CH_TOTAL_TRADES").asInt(0));
-			stock.setDeliveryTrade(item.path("COP_DELIV_QTY").asLong(0L));
-			stock.setDeliveryPercentage(item.path("COP_DELIV_PERC").asDouble(0.0));
-			stock.setVwap(item.path("VWAP").asDouble(0.0));
-
-			produceConsumePdv(stock, lastTicker);
 			stockList.add(stock);
+			this.kafkaProducerService.sendMessage(item.toPrettyString());
+
 		}
 		return stockList;
 	}
 
-	private void produceConsumePdv(PriceDeliveryVolume stock, AtomicReference<String> lastTicker) {
-		// Create topic for consumption
-		if (lastTicker.get() == null) {
-			this.kafkaUtils.createTopic(stock.getTicker());
-			lastTicker.set(stock.getTicker());
+	// Mapper Utility function
+	public PriceDeliveryVolume parseStockData(JsonNode node) {
 
-			// Consume the PDV data
-			this.kafkaUtils.consumePDVforATR(lastTicker.get());
-		}
+		PriceDeliveryVolume stock = new PriceDeliveryVolume();
+		stock.setTicker(node.path("CH_SYMBOL").asText(""));
+		stock.setDate(parseDate(node.path("mTIMESTAMP").asText("")));
+		stock.setHigh(node.path("CH_TRADE_HIGH_PRICE").asDouble(0.0));
+		stock.setLow(node.path("CH_TRADE_LOW_PRICE").asDouble(0.0));
+		stock.setOpen(node.path("CH_OPENING_PRICE").asDouble(0.0));
+		stock.setClose(node.path("CH_CLOSING_PRICE").asDouble(0.0));
+		stock.setLastTradeValue(node.path("CH_LAST_TRADED_PRICE").asDouble(0.0));
+		stock.setPrevoiusClosePrice(node.path("CH_PREVIOUS_CLS_PRICE").asDouble(0.0));
+		stock.setVolume(node.path("CH_TOT_TRADED_QTY").asLong(0L));
+		stock.setValue(node.path("CH_TOT_TRADED_VAL").asDouble(0.0));
+		stock.setTrades(node.path("CH_TOTAL_TRADES").asInt(0));
+		stock.setDeliveryTrade(node.path("COP_DELIV_QTY").asLong(0L));
+		stock.setDeliveryPercentage(node.path("COP_DELIV_PERC").asDouble(0.0));
+		stock.setVwap(node.path("VWAP").asDouble(0.0));
 
-		if (!lastTicker.get().equals(stock.getTicker())) {
-			this.kafkaUtils.createTopic(stock.getTicker());
-			lastTicker.set(stock.getTicker());
-
-			// Consume the PDV data
-			this.kafkaUtils.consumePDVforATR(lastTicker.get());
-		}
-
-		// Produce data for consumption
-		this.kafkaUtils.produceToStockTopic(stock.getTicker(), stock);
+		return stock;
 	}
 
 	// Helper function to parse dates safely
-	private LocalDate parseDate(String dateStr) {
+	public LocalDate parseDate(String dateStr) {
 		try {
 			return LocalDate.parse(dateStr, NSE_DATE_FORMATTER);
 		} catch (Exception e) {
-			return null; // Or LocalDate.now() or throw, based on your requirement
+			throw new DateParserException("Error occured in processing date : " + dateStr, e);
 		}
 	}
 }
