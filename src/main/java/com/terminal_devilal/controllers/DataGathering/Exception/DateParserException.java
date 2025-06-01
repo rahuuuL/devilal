409df49f25574afdf3ebbe493b5ce60d0692ce20
rahul <s.rahul0597@gmail.com>
2025-06-01 16:07:21 +0530

    added date parse exception

---
+++ src/main/java/com/terminal_devilal/controllers/DataGathering/Exception/DateParserException.java
@@ -0,0 +1,9 @@
+package com.terminal_devilal.controllers.DataGathering.Exception;
+
+@SuppressWarnings("serial")
+public class DateParserException extends RuntimeException {
+	public DateParserException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
