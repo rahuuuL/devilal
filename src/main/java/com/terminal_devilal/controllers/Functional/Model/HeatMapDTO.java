465274374aedc29119bf962f56bba5137c59f4bc
rahul <s.rahul0597@gmail.com>
2025-06-08 20:37:44 +0530

    delete unwanted files

--- src/main/java/com/terminal_devilal/controllers/Functional/Model/HeatMapDTO.java
+++
@@ -1,4 +0,0 @@
-package com.terminal_devilal.controllers.Functional.Model;
-
-public record HeatMapDTO(String stock, double openPrice, double closePrice, double percentChange) {
-}
--- src/main/java/com/terminal_devilal/controllers/Functional/Model/VolumeAnalysisDTO.java
+++
@@ -1,7 +0,0 @@
-package com.terminal_devilal.controllers.Functional.Model;
-
-import java.time.LocalDate;
-
-public record VolumeAnalysisDTO(String ticker, LocalDate date, long volume, double averageVolume,
-		double volumePercentChange, double deliveryPercentage) {
-}
