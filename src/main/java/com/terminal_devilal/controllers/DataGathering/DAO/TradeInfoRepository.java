acd74b40ccc5f7b3810678b07e2a80b6b148080c
rahul <s.rahul0597@gmail.com>
2025-07-18 04:58:15 +0530

    bounty hunting development

--- src/main/java/com/terminal_devilal/controllers/DataGathering/DAO/TradeInfoRepository.java
+++ src/main/java/com/terminal_devilal/controllers/DataGathering/DAO/TradeInfoRepository.java
@@ -1,10 +1,14 @@
 package com.terminal_devilal.controllers.DataGathering.DAO;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.terminal_devilal.controllers.DataGathering.Model.TickerDateId;
 import com.terminal_devilal.controllers.DataGathering.Model.TradeInfo;
 
-public interface TradeInfoRepository extends JpaRepository<TradeInfo, TickerDateId>  {
+public interface TradeInfoRepository extends JpaRepository<TradeInfo, TickerDateId> {
+
+	Optional<TradeInfo> findFirstByTickerOrderByDateDesc(String ticker);
 
 }
