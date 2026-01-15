39d40e7696222b3a7e3a30424941db5ce8a6dc65
rahul <s.rahul0597@gmail.com>
2026-01-15 13:16:49 +0530

    ticker details service development

---
+++ src/main/java/com/terminal_devilal/common/model/TickerDetails.java
@@ -0,0 +1,171 @@
+package com.terminal_devilal.common.model;
+
+import java.time.LocalDate;
+
+public class TickerDetails {
+
+	private String isin;
+
+	private String macro;
+
+	private String sector;
+
+	private String industry;
+
+	private String basicIndustry;
+
+	private String ticker;
+
+	private LocalDate date;
+
+	private double totalTradedVolume;
+
+	private double totalTradedValue;
+
+	private double totalMarketCap;
+
+	private double ffmc;
+
+	private double impactCost;
+
+	private double cmDailyVolatility;
+
+	private double cmAnnualVolatility;
+
+	public String getIsin() {
+		return isin;
+	}
+
+	public void setIsin(String isin) {
+		this.isin = isin;
+	}
+
+	public String getMacro() {
+		return macro;
+	}
+
+	public void setMacro(String macro) {
+		this.macro = macro;
+	}
+
+	public String getSector() {
+		return sector;
+	}
+
+	public void setSector(String sector) {
+		this.sector = sector;
+	}
+
+	public String getIndustry() {
+		return industry;
+	}
+
+	public void setIndustry(String industry) {
+		this.industry = industry;
+	}
+
+	public String getBasicIndustry() {
+		return basicIndustry;
+	}
+
+	public void setBasicIndustry(String basicIndustry) {
+		this.basicIndustry = basicIndustry;
+	}
+
+	public String getTicker() {
+		return ticker;
+	}
+
+	public void setTicker(String ticker) {
+		this.ticker = ticker;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public double getTotalTradedVolume() {
+		return totalTradedVolume;
+	}
+
+	public void setTotalTradedVolume(double totalTradedVolume) {
+		this.totalTradedVolume = totalTradedVolume;
+	}
+
+	public double getTotalTradedValue() {
+		return totalTradedValue;
+	}
+
+	public void setTotalTradedValue(double totalTradedValue) {
+		this.totalTradedValue = totalTradedValue;
+	}
+
+	public double getTotalMarketCap() {
+		return totalMarketCap;
+	}
+
+	public void setTotalMarketCap(double totalMarketCap) {
+		this.totalMarketCap = totalMarketCap;
+	}
+
+	public double getFfmc() {
+		return ffmc;
+	}
+
+	public void setFfmc(double ffmc) {
+		this.ffmc = ffmc;
+	}
+
+	public double getImpactCost() {
+		return impactCost;
+	}
+
+	public void setImpactCost(double impactCost) {
+		this.impactCost = impactCost;
+	}
+
+	public double getCmDailyVolatility() {
+		return cmDailyVolatility;
+	}
+
+	public void setCmDailyVolatility(double cmDailyVolatility) {
+		this.cmDailyVolatility = cmDailyVolatility;
+	}
+
+	public double getCmAnnualVolatility() {
+		return cmAnnualVolatility;
+	}
+
+	public void setCmAnnualVolatility(double cmAnnualVolatility) {
+		this.cmAnnualVolatility = cmAnnualVolatility;
+	}
+
+	public TickerDetails(String isin, String macro, String sector, String industry, String basicIndustry, String ticker,
+			LocalDate date, double totalTradedVolume, double totalTradedValue, double totalMarketCap, double ffmc,
+			double impactCost, double cmDailyVolatility, double cmAnnualVolatility) {
+		super();
+		this.isin = isin;
+		this.macro = macro;
+		this.sector = sector;
+		this.industry = industry;
+		this.basicIndustry = basicIndustry;
+		this.ticker = ticker;
+		this.date = date;
+		this.totalTradedVolume = totalTradedVolume;
+		this.totalTradedValue = totalTradedValue;
+		this.totalMarketCap = totalMarketCap;
+		this.ffmc = ffmc;
+		this.impactCost = impactCost;
+		this.cmDailyVolatility = cmDailyVolatility;
+		this.cmAnnualVolatility = cmAnnualVolatility;
+	}
+
+	public TickerDetails() {
+		super();
+	}
+
+}
---
+++ src/main/java/com/terminal_devilal/common/service/TickerInfoService.java
@@ -0,0 +1,65 @@
+package com.terminal_devilal.common.service;
+
+import org.springframework.stereotype.Service;
+
+import com.terminal_devilal.business_tools.trade_info.repository.TradeInfoRepository;
+import com.terminal_devilal.common.model.TickerDetails;
+import com.terminal_devilal.core_processes.sync_data.repository.TickerIndustryInfoRepository;
+
+@Service
+public class TickerInfoService {
+	private final TickerIndustryInfoRepository sectorDetails;
+
+	private final TradeInfoRepository infoRepository;
+
+	public TickerInfoService(TickerIndustryInfoRepository sectorDetails, TradeInfoRepository infoRepository) {
+		super();
+		this.sectorDetails = sectorDetails;
+		this.infoRepository = infoRepository;
+	}
+
+	/**
+	 * Enrich any TickerDetails subclass with sector + trade info
+	 */
+	public <T extends TickerDetails> T enrichTickerDetails(String ticker, T target) {
+
+		// ---- Trade info ----
+		infoRepository.findFirstByTickerOrderByDateDesc(ticker).ifPresentOrElse(trade -> {
+			target.setDate(trade.getDate());
+			target.setTotalTradedVolume(trade.getTotalTradedVolume());
+			target.setTotalTradedValue(trade.getTotalTradedValue());
+			target.setTotalMarketCap(trade.getTotalMarketCap());
+			target.setFfmc(trade.getFfmc());
+			target.setImpactCost(trade.getImpactCost());
+			target.setCmDailyVolatility(trade.getCmDailyVolatility());
+			target.setCmAnnualVolatility(trade.getCmAnnualVolatility());
+		}, () -> {
+			target.setDate(null);
+			target.setTotalTradedVolume(0.0);
+			target.setTotalTradedValue(0.0);
+			target.setTotalMarketCap(0.0);
+			target.setFfmc(0.0);
+			target.setImpactCost(0.0);
+			target.setCmDailyVolatility(0.0);
+			target.setCmAnnualVolatility(0.0);
+		});
+
+		// ---- Sector info ----
+		sectorDetails.findById(ticker).ifPresentOrElse(sector -> {
+			target.setIsin(sector.getIsin());
+			target.setMacro(sector.getMacro());
+			target.setSector(sector.getSector());
+			target.setIndustry(sector.getIndustry());
+			target.setBasicIndustry(sector.getBasicIndustry());
+		}, () -> {
+			target.setIsin("");
+			target.setMacro("");
+			target.setSector("");
+			target.setIndustry("");
+			target.setBasicIndustry("");
+		});
+
+		return target;
+	}
+
+}
