55ced2523823cf147b1db793125a6e60b8968aac
rahul <s.rahul0597@gmail.com>
2026-02-14 14:38:18 +0530

    added date in ticker value

--- src/main/java/com/terminal_devilal/indicators/common_entities/TickerValue.java
+++ src/main/java/com/terminal_devilal/indicators/common_entities/TickerValue.java
@@ -1,11 +1,16 @@
 package com.terminal_devilal.indicators.common_entities;
 
+import java.time.LocalDate;
+
 public class TickerValue {
 	private String ticker;
+	private LocalDate date;
 	private double value;
 
-	public TickerValue(String ticker, double value) {
+	public TickerValue(String ticker, LocalDate date, double value) {
+		super();
 		this.ticker = ticker;
+		this.date = date;
 		this.value = value;
 	}
 
@@ -13,8 +18,24 @@ public class TickerValue {
 		return ticker;
 	}
 
+	public void setTicker(String ticker) {
+		this.ticker = ticker;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
 	public double getValue() {
 		return value;
+	}
+
+	public void setValue(double value) {
+		this.value = value;
 	}
 
 }
