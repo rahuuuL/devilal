2484c6d817ec8bcb219421ecd8564201dbf24b03
rahuuuL <s.rahul0597@gmail.com>
2026-04-13 20:33:09 +0530

    kafka consumer producer to take the kafka as per located on system

--- src/main/java/com/terminal_devilal/configurations/kakfa/KafkaConsumerConfig.java
+++ src/main/java/com/terminal_devilal/configurations/kakfa/KafkaConsumerConfig.java
@@ -3,12 +3,15 @@ package com.terminal_devilal.configurations.kakfa;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
@@ -16,10 +19,13 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 @EnableKafka
 public class KafkaConsumerConfig {
 
+	@Value("${spring.kafka.bootstrap-servers}")
+	private String bootstrapServers;
+
 	@Bean
 	public ConsumerFactory<String, String> consumerFactory() {
 		Map<String, Object> consumerProps = new HashMap<>();
-		consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 		consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, "devilal-group");
 		consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 		consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
@@ -33,6 +39,11 @@ public class KafkaConsumerConfig {
 		ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
 		factory.setConsumerFactory(consumerFactory);
 		return factory;
+	}
+
+	@Bean
+	public NewTopic pdvTopic() {
+		return TopicBuilder.name("pdv-data").partitions(32).replicas(1).build();
 	}
 
 }
--- src/main/java/com/terminal_devilal/configurations/kakfa/KafkaProducerConfig.java
+++ src/main/java/com/terminal_devilal/configurations/kakfa/KafkaProducerConfig.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -16,18 +17,20 @@ import org.springframework.kafka.core.ProducerFactory;
 @EnableKafka
 public class KafkaProducerConfig {
 
+	@Value("${spring.kafka.bootstrap-servers}")
+	private String bootstrapServers;
+
 	@Bean
 	public KafkaTemplate<String, String> kafkaTemplate() {
 		Map<String, Object> producerProps = new HashMap<>();
-		producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 		producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
 		// Avro specific
-		producerProps.put("schema.registry.url", "http://localhost:8081");
+//		producerProps.put("schema.registry.url", "http://localhost:8081");
 
-		ProducerFactory<String, String> producerFactory = new DefaultKafkaProducerFactory<>(
-				producerProps);
+		ProducerFactory<String, String> producerFactory = new DefaultKafkaProducerFactory<>(producerProps);
 		return new KafkaTemplate<String, String>(producerFactory);
 	}
 
