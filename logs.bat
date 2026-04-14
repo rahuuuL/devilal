533c32da75c793e238db2a83f345ebc1a28beda5
rahuuuL <s.rahul0597@gmail.com>
2026-04-14 12:27:13 +0530

    kafka config for spring boot and sql performance boost

--- logs.bat
+++ logs.bat
@@ -6,6 +6,7 @@ echo Opening Docker logs in separate windows...
 start "JAVA LOGS" cmd /k "docker compose logs -f java"
 start "ANGULAR LOGS" cmd /k "docker compose logs -f angular"
 start "PYTHON LOGS" cmd /k "docker compose logs -f python"
+start "KAFKA LOGS" cmd /k "docker compose logs -f kafka"
 
 echo Done!
 pause
