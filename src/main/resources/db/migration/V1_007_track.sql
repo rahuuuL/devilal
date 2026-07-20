accfed74c83428266ef92eddce5910662f46bab0
rahuuuL <s.rahul0597@gmail.com>
2026-07-20 23:09:56 +0530

    Updated the db name to devil_db

--- src/main/resources/db/migration/V1_007_track.sql
+++ src/main/resources/db/migration/V1_007_track.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `stonks` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `stonks`;
+CREATE DATABASE  IF NOT EXISTS `devil_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `devil_db`;
 -- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: stonks
+-- Host: 127.0.0.1    Database: devil_db
 -- ------------------------------------------------------
 -- Server version	8.4.8
 
